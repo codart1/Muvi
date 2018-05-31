3fd18701b2af50f57fdf1c357f73a775ddf3904a
Codart <domanhcp@gmail.com>
2018-06-01 01:31:13 +0700

    has some fun

--- src/render-entities/dot.js
+++ src/render-entities/dot.js
@@ -16,6 +16,7 @@ export default class Dot extends BaseRenderEntity {
   dots = [];
   texture = createTexture();
   container = new PIXI.Container();
+  MAX_VAL = 150;
 
   init() {
     const data = this.visualizer.data;
@@ -26,11 +27,11 @@ export default class Dot extends BaseRenderEntity {
       dot.x = Math.random() * width;
       dot.y = Math.random() * height;
 
-      const dX = Math.random() * width - dot.x
-      const dY = Math.random() * height - dot.y
+      const dX = Math.random() * width - dot.x;
+      const dY = Math.random() * height - dot.y;
 
       dot.angle = Math.atan2(dX, dY);
-      dot.velocity = 400 * Math.random();
+      dot.velocity = 50;
 
       return dot;
     });
@@ -42,14 +43,32 @@ export default class Dot extends BaseRenderEntity {
 
   onTick(delta) {
     const deltaSecond = delta / 1000;
-    
-    this.dots.forEach(item => {
-      const v = item.velocity * deltaSecond
+    const data = this.visualizer.data;
+    const { width, height } = this.app.screen;
+
+    this.dots.forEach((item, i) => {
+      const value = data[i],
+        v = (item.velocity + value * 10) * deltaSecond,
+        alpha = value > this.MAX_VAL ? 1 : 1 / this.MAX_VAL * value;
 
       item.x += v * Math.cos(item.angle);
       item.y += v * Math.sin(item.angle);
-    });
 
+      item.alpha = alpha;
+      // item.scale.set(alpha * 4 + 1, alpha * 4 + 1);
+
+      if (item.x > width) {
+        item.x = 0;
+      } else if (item.x < 0) {
+        item.x = width;
+      }
+
+      if (item.y > height) {
+        item.y = 0;
+      } else if (item.y < 0) {
+        item.y = height;
+      }
+    });
     // const {x, y, velocity} = this.dots[0]
     // console.log({x, y, velocity, deltaSecond})
   }
