ae8659fe1a6a3912a31ce065b9b587bc4320ec47
Codart <domanhcp@gmail.com>
2018-06-05 00:39:55 +0700

    add upload

--- src/index.js
+++ src/index.js
@@ -14,3 +14,40 @@ $("#play").on("click", () => {
     visualizer = new Visualizer(analyser, $("#canvasContainer")[0]);
   $audio[0].play();
 });
+
+$("#upload").on("click", function() {
+  const $input = $(this).find("input");
+
+  // console.log(this)
+
+  // $input.trigger("click");
+
+  // $("input").on("change", function(e) {
+  //   +  var target = e.currentTarget;
+  //   +  var file = target.files[0];
+  //   +
+  //   +  const url = URL.createObjectURL(file);
+  //   +
+  //   +  $audio.attr("src", url);
+  //   +
+  //   +  const analyser = new Analyser($("audio")[0]),
+  //   +    visualizer = new Visualizer(analyser, $("#canvasContainer")[0]);
+  //   +});
+});
+
+$("#pickSong").on("change", function(e) {
+  const target = e.currentTarget;
+  console.log(target.files)
+  const file = target.files[0];
+
+
+  const url = URL.createObjectURL(file);
+
+  $audio.attr("src", url);
+
+  const analyser = new Analyser($("audio")[0]),
+    visualizer = new Visualizer(analyser, $("#canvasContainer")[0]);
+  $audio[0].play();
+
+  $("#start").addClass("hide");
+});
